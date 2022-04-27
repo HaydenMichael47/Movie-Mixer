42725c08825a18638e1f254104f44e421ca73d26
Hayden Michael <haydentmichael@gmail.com>
2022-04-26 22:07:19 -0400

    Added Home page, Movie page, and Login page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,18 +11,22 @@ import {HttpClient } from '@angular/common/http'
 export class HomeComponent implements OnInit {
   trendingMovies:any;
   poster: any;
+  movieID: any;
+
+  popularMovies: any;
   
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.getTrendingMovies();
+    this.getPopularMovies();
   }
 
   getTrendingMovies(){
-    this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=d49eaf1ed2c2fbde095c2615c53504fa&language=en-US&page=1").subscribe((movies: any) =>{
+    this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=d49eaf1ed2c2fbde095c2615c53504fa&language=en-US&page=2").subscribe((movies: any) =>{
       this.trendingMovies = movies.results;
-       
+      this.movieID = movies.results.id;
 
      
       console.log( this.trendingMovies);
@@ -31,6 +35,21 @@ export class HomeComponent implements OnInit {
     })
 
   }
+  getPopularMovies(){
+    this.http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=d49eaf1ed2c2fbde095c2615c53504fa&language=en-US&page=2").subscribe((movies: any) =>{
+      this.popularMovies = movies.results;
+      this.movieID = movies.results.id;
 
+     
+      console.log( this.popularMovies);
+      
+      console.log();
+    })
+
+  }
+
+  goToMovie(type: string, id: String){
+      this.router.navigate(['movie', type,id]);
+  }
  
 }
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
-  constructor() { }
+
+  type = '';
+  id = '';
+  url = '';
+  movies: any;
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.type = this.route.snapshot.params['type']
+    this.id = this.route.snapshot.params['id']
+    this.getMovie();
   }
 
+
+  getMovie(){
+    this.http.get("https://api.themoviedb.org/3/movie/" + this.id + "?api_key=d49eaf1ed2c2fbde095c2615c53504fa&language=en-US&page=1").subscribe((movies) =>{
+      this.movies = movies;
+    })
+  }
 }
